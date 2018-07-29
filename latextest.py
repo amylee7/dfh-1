c3d107ec179f032c73a77b9c1397eb65c6310220
A Lee <40435212+al3393@users.noreply.github.com>
2018-07-28 22:32:17 -0400

    Update latextest.py

--- latextest.py
+++ latextest.py
@@ -9,7 +9,6 @@ class LatexTest(unittest.TestCase):
     def testPrintDAStructure(self):
         
         dic = {(4,4):(3.5,4), (3.5,4):(3,4), (3,2):(3.5,2), (3.5,2):(4,3), (4,2):(3.5,3), (3.5,3):(3,3), (3,1):(3.5,1), (3.5,1):(4,1)}
-#        dic = {(4,4):(3.5,4),(3.5,4):(3,4),(4,2):(3.5,3), (3.5,3):(3,3), (3,2):(3,1)}
         tang = TANGLE(dic)
         strands =  (((3,4),(2,1),(1,2)),((3,2),(0,1)))
         right_alg = StrandAlgebra(F2, tang, False)
